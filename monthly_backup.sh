97e16e799dabccc531cbd7ddd2dc309292b0afb5
iamFatur13 <faturochman.ilham@gmail.com>
2026-04-14 15:48:19 +0800

    Update monthly_backup.sh

--- monthly_backup.sh
+++ monthly_backup.sh
@@ -3,19 +3,13 @@
 
 #TODAY=$(date +%Y-%m-%d-%H-%M-%S)
 TODAY=$(date +%Y-%m-%d)
-DATADIR=/volume1/*
-BACKUPDIR=/volume3/Monthly_backup
-SCRIPTDIR=/mnt/script_backup
+DATADIR=/path/sumber/data/yang/akan/di/backup/*
+BACKUPDIR=/path/tujuan/backup
 LASTDAYPATH=${BACKUPDIR}/$(ls ${BACKUPDIR} | tail -n 1)
 TODAYPATH=${BACKUPDIR}/${TODAY}
 if [[ ! -e ${TODAYPATH} ]]; then
         mkdir -p ${TODAYPATH}
 fi
 
-#rsync -av --link-dest ${LASTDAYPATH} ${DATADIR} ${TODAYPATH} $@
 rsync -avm --exclude-from='/mnt/script_backup/exclude-list.txt' --backup --backup-dir=${LASTDAYPATH} ${DATADIR} ${TODAYPATH} $@
 
-#tar -czvf $TODAY.tar.gz ${DATADIR}
-#mv ${DATADIR}${TODAY}.tar.gz ${BACKUPDIR} 
-
-#${SCRIPTDIR}/deleteOldBackups.sh
